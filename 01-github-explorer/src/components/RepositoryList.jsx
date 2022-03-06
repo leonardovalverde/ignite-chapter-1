e16355c8708271d5139b33349b0d8eff697f1f23
Leonardo Valgreen <leovmsi@hotmail.com>
2022-03-06 17:33:41 -0300

    feat: hooks

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,13 +1,24 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>List de respositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>form in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
